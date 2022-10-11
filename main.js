2cdd65df7f85664dff527bc8015f976334346913
OrochimaruBotz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-10-11 13:30:53 +0700

    Update main.js

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ global.DATABASE = global.db // Backwards Compatibility
 global.conn = new WAConnection()
 conn.version =[2,2143,3]
 conn.browserDescription =['Kanna-Bot', 'Deskop', '3.0']
-let authFile = `${opts._[0] || 'session'}.data.json`
+let authFile = `${opts._[0] || 'kannabot'}.data.json`
 if (fs.existsSync(authFile)) conn.loadAuthInfo(authFile)
 if (opts['trace']) conn.logger.level = 'trace'
 if (opts['debug']) conn.logger.level = 'debug'
